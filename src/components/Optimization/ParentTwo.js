3c490f6a48b88ce3fd34ab265919533b7654da8c
Njiraini23 <njiraini88@gmail.com>
2024-09-14 19:00:41 +0300

    React Rendering and Optimization

---
+++ src/components/Optimization/ParentTwo.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react'
+import { MemoizedChildTwo} from './ChildTwo'
+
+export const ParentTwo = () => {
+    const [count, setCount] = useState(0)
+    const [name, setName] = useState('Vishwas') 
+
+    console.log('ParentTwo Render')
+  return (
+    <div> 
+      <button onClick={() => setCount( c => c + 1)}>Count - {count}</button> 
+      <button onClick={() => setName('Codevolution')}>Change name</button>
+      <MemoizedChildTwo name={name} />
+    </div>
+  )
+}
+
+
+ 
